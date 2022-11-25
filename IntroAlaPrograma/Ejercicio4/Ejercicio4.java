a66d332ca552a4d2dd4cc5ec1281d2e11fcdf01b
frevar1975 <55697584+frevar1975@users.noreply.github.com>
2022-11-25 18:35:05 -0400

    Initial commit

---
+++ IntroAlaPrograma/Ejercicio4/Ejercicio4.java
@@ -0,0 +1,22 @@
+//
+import java.util.function.Function;
+
+public class Ejercicio4{
+/**
+ * @param args
+ */
+public static void main(String[] args) {
+   // var numeroIf = 0;
+        //resultant= espositivo(3);
+        //System.out.println(resultant);
+       
+   }
+    public static int espositivo(final int numero)
+    {
+        if (numero > 0){
+            System.out.println("Es positivo");
+        }
+        return numero;
+    }
+}
+
