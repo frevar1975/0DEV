d2224b067caef3e703f77ba809819507fd2d7c1a
frevar1975 <55697584+frevar1975@users.noreply.github.com>
2022-11-30 12:16:17 -0400

    Create Tema1entrega.java

---
+++ IntroAlaPrograma/Tema1entrega.java
@@ -0,0 +1,21 @@
+package IntroAlaPrograma;
+
+public class Tema1entrega {
+    public static void main(String[] args) {
+            /* NUMEROS*/
+            System.out.println("entrega tarea 1");
+            byte variable = 0;
+            short variable1 = 10;
+            int variable2 = 100;
+            long variable3 = 100000;
+    
+            /* flotante*/
+            float variable4 = 1.3f;
+            double variable5= 5.5d;
+            /*booleano*/
+            boolean variable6=true;
+            /*texto*/
+            char variable7='a';
+            String variable8= "hola";
+    }
+}
