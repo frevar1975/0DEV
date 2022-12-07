653b6f815e7367ca658d693cf6ac58a858fe9de5
frevar1975 <55697584+frevar1975@users.noreply.github.com>
2022-12-06 22:02:49 -0400

    Create Entregatem10.java
    ultima tarea

---
+++ IntroAlaPrograma/Ejercicio10/Entregatem10.java
@@ -0,0 +1,15 @@
+package IntroAlaPrograma.Ejercicio10;
+
+import IntroAlaPrograma.Clases.main;
+
+public class Entregatem10 {
+public static void main(String[] args) {
+    
+}
+
+
+public static String reverse(String texto) {
+    return texto;
+    
+ }
+}
