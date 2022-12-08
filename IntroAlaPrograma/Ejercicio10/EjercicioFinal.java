05a94508e0d78b9425b8cc20802f0c1db41cdcbc
frevar1975 <55697584+frevar1975@users.noreply.github.com>
2022-12-07 21:12:22 -0400

    adasd

--- IntroAlaPrograma/Ejercicio10/EjercicioFinal.java
+++ IntroAlaPrograma/Ejercicio10/EjercicioFinal.java
@@ -34,7 +34,9 @@ public class EjercicioFinal {
 
          /*Utilizando InputStream y PrintStream, crea una función que reciba dos parámetros: "fileIn" y "fileOut".
          La tarea de la función será realizar la copia del fichero dado en el parámetro "fileIn" al fichero dado en "fileOut". */
-        try {
+        
+         // desde casa 
+         try {
          InputStream fichero = new FileInputStream("c:/dev/filein.txt");
          try{
             byte[] datos = fichero.readAllBytes();
